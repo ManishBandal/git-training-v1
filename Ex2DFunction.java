2f7b4bdea9d6fc56b3b8b515f4abff4b659d7d74
ManishBandal <bandalmanish@gmail.com>
2025-06-07 10:41:46 +0530

    2nd commit

---
+++ Ex2DFunction.java
@@ -0,0 +1,11 @@
+public class Ex2DFunction {
+    public static void main(String[] args) {
+        int number = 5; // Example input
+        int result = twice(number);
+        System.out.println("Twice of " + number + " is " + result);
+    }
+
+    public static int twice(int n) {
+        return n * 2;
+    }
+}
