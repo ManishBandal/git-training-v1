55091356f945adbff0945bd48be3973219d7a332
ManishBandal <bandalmanish@gmail.com>
2025-06-12 21:41:18 +0530

    Modified ex2 file from devteam2 branch

--- Ex2DFunction.java
+++ Ex2DFunction.java
@@ -3,6 +3,9 @@ public class Ex2DFunction {
         int number = 5; // Example input
         int result = twice(number);
         System.out.println("Twice of " + number + " is " + result);
+
+        // Added a new line to demonstrate changes in devteam2 branch
+        System.out.println("Added this from devteam2 branch");
     }
 
     public static int twice(int n) {
