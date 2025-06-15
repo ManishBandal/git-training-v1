2d73aae2bd03a0a41387799ad4146668c6e4db3a
ManishBandal <bandalmanish@gmail.com>
2025-06-15 09:29:19 +0530

    added a variable in ex2 from techdev branch

--- Ex2DFunction.java
+++ Ex2DFunction.java
@@ -4,5 +4,8 @@ public class Ex2DFunction {
     public static void main(String[] args) {
        System.out.println("Hello, World!"); // Print a greeting message
        System.out.println("new commit from techdev branch"); // Print a message indicating a new commit
+        int a = 5; // Initialize variable a
+        
+    
     }
 }
