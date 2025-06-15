8981dfa7527087027a18aa8c9f954dc86c41950b
ManishBandal <bandalmanish@gmail.com>
2025-06-15 09:27:43 +0530

    new print fom techdev brnach i ex2

--- Ex2DFunction.java
+++ Ex2DFunction.java
@@ -3,6 +3,6 @@ public class Ex2DFunction {
     // Main method to run the program
     public static void main(String[] args) {
        System.out.println("Hello, World!"); // Print a greeting message
-       
+       System.out.println("new commit from techdev branch"); // Print a message indicating a new commit
     }
 }
