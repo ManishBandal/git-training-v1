a9e5e8f0a1c65bed9511d199b8aa0120743b7b7e
ManishBandal <bandalmanish@gmail.com>
2025-06-15 09:16:58 +0530

    updated ex3 file from techdev

--- demofiles/ex3.java
+++ demofiles/ex3.java
@@ -1,1 +1,9 @@
 //added file from github website
+public class ex3 {
+    public static void main(String[] args) {
+        // Create an array of integers
+        System.out.println("made change from techdev branch");
+        System.out.println("this is a new line");
+        }
+    
+}
